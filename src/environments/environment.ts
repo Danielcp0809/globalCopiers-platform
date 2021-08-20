b3f14f9ad0cd6fe502fdd4adb776b24ba2ef9acc
Daniel Córdova <jdany0809@hotmail.com>
2021-08-20 18:52:32 -0500

    Implementacion de responsive al header general

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyAmTQg-BSfqhfRrlsdMJJ3pfQQQt9IQ2KA",
-    authDomain: "globalcopiesdev.firebaseapp.com",
-    projectId: "globalcopiesdev",
-    storageBucket: "globalcopiesdev.appspot.com",
-    messagingSenderId: "293069129389",
-    appId: "1:293069129389:web:52a811f6cd39ef05c2e824",
-    measurementId: "G-X4L08R3M51"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
 
